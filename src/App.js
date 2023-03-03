6ab504431ad4dfb01b1851a33685e546b56f0511
Anvesh <anveshvanam5@gmail.com>
2023-03-03 20:18:29 +0000

    add NotFound page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import ProtectedRoute from './components/ProtectedRoute'
 import Home from './components/Home'
 import BookItemDetails from './components/BookItemDetails'
 import LoginPage from './components/LoginPage'
 import Bookshelves from './components/Bookshelves'
+import NotFound from './components/NotFound'
 import './App.css'
 
 // use the below bookshelvesList for rendering read status of book items in Bookshelves Route
@@ -14,6 +15,8 @@ const App = () => (
     <Route exact path="/login" component={LoginPage} />
     <ProtectedRoute exact path="/shelf" component={Bookshelves} />
     <ProtectedRoute exact path="/books/:id" component={BookItemDetails} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
   </Switch>
 )
 
---
+++ src/components/NotFound/index.js
@@ -0,0 +1,36 @@
+import {Link, withRouter} from 'react-router-dom'
+
+import './index.css'
+
+const NotFound = props => {
+  const onClickBackToHome = () => {
+    const {history} = props
+    history.replace('/')
+  }
+
+  return (
+    <div className="not-found-container">
+      <img
+        src="https://res.cloudinary.com/dq9eefxnb/image/upload/v1677406354/notfound_y5jqxw.png"
+        alt="not found"
+        className="not-found-image"
+      />
+      <h1 className="not-found-heading">Page Not Found</h1>
+      <p className="not-found-para">
+        we are sorry, the page you requested could not be found, please go back
+        to the homepage
+      </p>
+      <Link to="/">
+        <button
+          type="button"
+          className="not-found-button"
+          onClick={onClickBackToHome}
+        >
+          Go Back to Home
+        </button>
+      </Link>
+    </div>
+  )
+}
+
+export default withRouter(NotFound)
